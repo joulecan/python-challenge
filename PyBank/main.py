265957302ae715e159a488a293c02cdbdce2e344
joulecan <jol4can@gmail.com>
2022-07-17 23:31:35 +0800

    Cleaned up the code at line 21.

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,9 +3,9 @@ import os
 
 budget_csvfile = os.path.join("Resources", "budget_data.csv")
 
-#Variables for 
-total_profLoss = float
-profLossChange = float
+#Variables declared
+total = 0
+changeTotal = 0
 Gincrease = float
 Gdecrease = float
 monthList = []
@@ -15,12 +15,16 @@ with open(budget_csvfile) as budcsvfile:
     budgetReader = csv.reader(budcsvfile, delimiter=',')
     #print(budgetReader)
     csvHeader = next(budgetReader)
-    #print(f"CSV Header: {csvHeader}")
+    print(f"CSV Header: {csvHeader}")
     for row in budgetReader:
         monthList.append(row[0])
-        
-        #print(monthList)
-        #print(row)
+        total += int(row[1])
+        changeTotal = int(row[1])
+
+        print(row)
+
     print("Financial Analysis")
-    print("-"*30)
-    print(f'Total Number of Months: {len(monthList)}')
+    print("-"*25)
+    print(f'Total No. of Months: {len(monthList)}')
+    print(f'Gross Value: ${total}')
+    print(f'Average Change: ${changeTotal}')
