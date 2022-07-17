c006966e0f7d55e97b7645b8a954d9cc815e607e
joulecan <jol4can@gmail.com>
2022-07-17 16:54:44 +0800

    Have results for total months in main.py.

---
+++ PyBank/main.py
@@ -0,0 +1,27 @@
+import csv
+import os
+
+budget_csvfile = os.path.join("Resources", "budget_data.csv")
+
+#Variables for 
+total_months = int
+total_profLoss = float
+profLossChange = float
+Gincrease = float
+Gdecrease = float
+monthList = []
+
+with open(budget_csvfile) as budcsvfile:
+
+    budgetReader = csv.reader(budcsvfile, delimiter=',')
+    #print(budgetReader)
+    csvHeader = next(budgetReader)
+    #print(f"CSV Header: {csvHeader}")
+    for row in budgetReader:
+        monthList.append(row[0])
+        
+        #print(monthList)
+        #print(row)
+    print("Financial Analysis")
+    print("-"*30)
+    print(f'Total Number of Months: {len(monthList)}')
